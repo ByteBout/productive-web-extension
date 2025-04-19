140f6e5005c890a680ae5f664fcf4fa906736922
Sepehr Saemi <sepehrsaemi@hotmail.com>
2025-04-19 21:49:46 +0300

    style: implement Unhook tab, Youtube specific popup design

--- build/popup/popup.js
+++ build/popup/popup.js
@@ -1,3 +1,6 @@
+// HTML Elements
+const unhookTabContentEl = document.querySelector("#unhook-tab-content");
+
 // Check active tab URL and extract sld from it
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const url = new URL(tabs[0].url).hostname;
@@ -17,4 +20,98 @@ function loadPopupContent(sld) {
     }
 }
 
-function loadYoutubeContent() {}
+function loadYoutubeContent() {
+    unhookTabContentEl.innerHTML = `
+            <fieldset class="fieldset bg-base-200 rounded-box border-base-300 -mt-3 mr-2 w-full border px-4 py-2.5">
+                <legend class="fieldset-legend">Home Page</legend>
+                <label class="label flex justify-between px-2">
+                    Feed
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Shorts
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Lives
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label mb-1.5 flex justify-between px-2">
+                    Ads
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+            </fieldset>
+
+            <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-full border px-4 py-2.5">
+                <legend class="fieldset-legend px-1">Sidebar</legend>
+                <label class="label flex justify-between px-2">
+                    Subscriptions
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Explore
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label mb-1.5 flex justify-between px-2">
+                    More from Youtube
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+            </fieldset>
+
+            <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-full border px-4 py-2.5">
+                <legend class="fieldset-legend px-1">Video Page</legend>
+                <label class="label flex justify-between px-2">
+                    Title
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Channel Info
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Action Buttons
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Description
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Comments
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Live Chat
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label mb-1.5 flex justify-between px-2">
+                    Recommends
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+            </fieldset>
+
+            <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-full border px-4 py-2.5">
+                <legend class="fieldset-legend px-1">Channel Page</legend>
+                <label class="label flex justify-between px-2">
+                    Posts Tab
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Shorts Tab
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Live Tab
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Store Tab
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+                <label class="label mb-1.5 flex justify-between px-2">
+                    Podcast Tab
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" />
+                </label>
+            </fieldset>
+    `;
+}
