caa3346d5cf2b7886101e6ded125c8cc919c9c8b
Sepehr Saemi <sepehrsaemi@hotmail.com>
2025-04-28 14:46:40 +0300

    style: implement Youtube's popup design

--- build/popup/popup.js
+++ build/popup/popup.js
@@ -1,3 +1,8 @@
+// HTML Elements
+const platformLogoEl = document.querySelector("#platform-logo");
+const platformNameEl = document.querySelector("#platform-name");
+const unhookTabContentEl = document.querySelector("#unhook-tab-content");
+
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     // Check active tab URL and extract sld from it.
     try {
@@ -13,7 +18,96 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 
 function loadPopupContent(sld) {
     // load popup content based on platform
-    const supportedPlatforms = {};
+    const supportedPlatforms = {
+        youtube: loadYoutubeContent,
+    };
 
     if (sld in supportedPlatforms) supportedPlatforms[sld]();
+}
+
+function loadYoutubeContent() {
+    platformLogoEl.src = "../../assets/images/youtube.png";
+    platformNameEl.textContent = "Youtube";
+
+    unhookTabContentEl.innerHTML = `
+            <fieldset class="fieldset bg-base-200 rounded-box border-base-300 -mt-3 mr-2 w-full border px-4 py-2.5">
+                <legend class="fieldset-legend">General</legend>
+                <label class="label flex justify-between px-2">
+                    Home Feed
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-feed" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Shorts
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-shorts" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Ads
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-ads" />
+                </label>
+                <label class="label mb-1.5 flex justify-between px-2">
+                    Notification
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-notification" />
+                </label>
+            </fieldset>
+
+            <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-full border px-4 py-2.5">
+                <legend class="fieldset-legend px-1">Sidebar</legend>
+                <label class="label flex justify-between px-2">
+                    Subscriptions
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-subscriptions" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Explore
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-explore" />
+                </label>
+                <label class="label mb-1.5 flex justify-between px-2">
+                    More from Youtube
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-more-from-youtube" />
+                </label>
+            </fieldset>
+
+            <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-full border px-4 py-2.5">
+                <legend class="fieldset-legend px-1">Video Page</legend>
+                <label class="label flex justify-between px-2">
+                    Title
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-video-title" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Channel Info
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-channel-info" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Action Buttons
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-action-buttons" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Description
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-description" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Shop
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-shop" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Comments
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-comments" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Recommends
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-recommends" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Live Chat
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-live-chat" />
+                </label>
+                <label class="label flex justify-between px-2">
+                    Playlist
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-playlist" />
+                </label>
+                <label class="label mb-1.5 flex justify-between px-2">
+                    Fundraise
+                    <input type="checkbox" class="toggle toggle-primary toggle-sm" id="hide-fundraise" />
+                </label>
+            </fieldset>
+    `;
 }
